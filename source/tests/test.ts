d874cef25e70fbdfdc1af726592f714b0c4a266c
Mythic <mythicmaniac@gmail.com>
2017-06-12 13:52:11 +0300

    Add a test for VoidnetServer and fix issues
    Add a test for VoidnetServer that attempts to connect two nodes together
    Fix issues that arised while running the test

--- source/tests/test.ts
+++ source/tests/test.ts
@@ -1,18 +1,30 @@
 import { suite, test } from "mocha-typescript"
 import { expect } from "chai"
 
-import { VoidnetNodeMeta } from "../voidnet"
+import { VoidnetNodeMeta, VoidnetServer } from "../voidnet"
 
 @suite("Test Voidnet Components")
 class Tests {
     @test
     "VoidnetNodeMeta"() {
-        const meta = new VoidnetNodeMeta("www.test.domain", 404)
+        const meta = new VoidnetNodeMeta({
+            hostname: "www.test.domain",
+            port: 404
+        })
         expect(meta.hostname).to.equal("www.test.domain")
         expect(meta.port).to.equal(404)
-        expect(meta.uri).to.equal("www.test.domain:404")
+        expect(meta.uri).to.equal("http://www.test.domain:404")
         expect(meta.guid).to.not.be.null
     }
+
+    @test
+    "VoidnetServer"(done) {
+        const srv1 = new VoidnetServer("localhost", 8081)
+        const srv2 = new VoidnetServer("localhost", 8082)
+        srv1.on("connection", () => {
+            expect(srv1.connectionCount).to.equal(1)
+            done()
+        })
+        srv1.Connect(srv2.meta.uri)
+    }
 }
-
-
--- source/voidnet.ts
+++ source/voidnet.ts
@@ -2,34 +2,43 @@ import * as SocketIOServer from "socket.io"
 import * as SocketIOClient from "socket.io-client"
 import * as http from "http"
 import * as Guid from "guid"
+import * as events from "events"
 
 export class VoidnetNodeMeta {
     public readonly hostname: string
     public readonly port: number
     public readonly guid: string
 
-    get uri(): string { return this.hostname + ":" + this.port }
+    get uri(): string { return "http://" + this.hostname + ":" + this.port }
 
-    constructor(hostname: string, port: number) {
-        this.hostname = hostname
-        this.port = port
-        this.guid = Guid.create().value;
+    constructor(data: {hostname: string, port: number, guid?: string}) {
+        this.hostname = data.hostname
+        this.port = data.port
+        this.guid = data.guid;
+        if(this.guid === undefined) {
+            this.guid = Guid.create().value;
+        }
+    }
+
+    ToHandshakeDatum(data: string): VoidnetHandshakeDatum {
+        return {
+            "hostname": this.hostname,
+            "port": this.port,
+            "guid": this.guid,
+            "data": data
+        }
     }
 }
 
-class VoidnetHandshakeDatum {
-    public readonly meta: VoidnetNodeMeta
-    public readonly extra: string
+function GetUri(datum: VoidnetHandshakeDatum): string {
+    return "http://" + datum.hostname + ":" + datum.port
+}
 
-    get hostname(): string { return this.meta.hostname }
-    get port(): number { return this.meta.port }
-    get guid(): string { return this.meta.guid }
-    get uri(): string { return this.meta.uri }
-
-    constructor(meta: VoidnetNodeMeta, extra: string) {
-        this.meta = meta
-        this.extra = extra
-    }
+interface VoidnetHandshakeDatum {
+    hostname: string
+    port: number
+    guid: string
+    data: string
 }
 
 // Voidnet Connection Handshake
@@ -59,7 +68,7 @@ function HandleHandshake(
         respond(localMeta)
 
         // Form a client connection to the node wanting to connect
-        const clientSocket = SocketIOClient(remoteHandshake.uri)
+        const clientSocket = SocketIOClient(GetUri(remoteHandshake))
         clientSocket.on("connect", () => {
 
             // Send the "handshake-ack" event with our meta
@@ -68,15 +77,15 @@ function HandleHandshake(
                 // Make sure we connected to the same node that connected to us
                 // Do this by verifying the meta and the secret are the same
                 const check = (
-                    remoteHandshake.uri == remoteVerify.uri &&
+                    GetUri(remoteHandshake) == GetUri(remoteVerify) &&
                     remoteHandshake.guid == remoteVerify.guid &&
-                    remoteHandshake.extra == remoteVerify.extra // Secret
+                    remoteHandshake.data == remoteVerify.data // Secret
                 )
 
                 // This wasn't the same node
                 // Inform the node the handshake failed and close connections
                 if(!check) {
-                    serverSocket.emit("handshake-result", new VoidnetHandshakeDatum(localMeta, "fail"))
+                    serverSocket.emit("handshake-result", localMeta.ToHandshakeDatum("fail"))
                     serverSocket.disconnect(true)
                     clientSocket.disconnect()
                     return
@@ -84,12 +93,11 @@ function HandleHandshake(
 
                 // This was the same node, handshake should be Successfull
                 // We now have connected to the node as the client and as the server
-                serverSocket.emit("handshake-result", new VoidnetHandshakeDatum(localMeta, "success"))
-                console.log("Successfull handshake")
+                serverSocket.emit("handshake-result", localMeta.ToHandshakeDatum("success"))
                 successfullHandshakeCallback(new VoidnetConnection(
                     clientSocket,
                     serverSocket,
-                    remoteHandshake.meta
+                    new VoidnetNodeMeta(remoteHandshake)
                 ))
             })
         })
@@ -127,28 +135,28 @@ class PendingHandshake {
     }
 }
 
-interface Dictionary<T> {
-    [x: string]: T
-}
-
 class VoidnetHandshakeHandler {
-    meta: VoidnetNodeMeta
-    pendingHandshakes: Dictionary<PendingHandshake>
-    handshakeSuccessHandler: Function
+    private readonly _meta: VoidnetNodeMeta
+    protected pendingHandshakes: Map<string, PendingHandshake>
+    protected handshakeSuccessHandler: Function
+
+    get meta(): VoidnetNodeMeta {
+        return this._meta
+    }
 
     constructor(meta: VoidnetNodeMeta, handshakeSuccessHandler: Function) {
-        this.meta = meta
-        this.pendingHandshakes = {}
+        this._meta = meta
+        this.pendingHandshakes = new Map()
         this.handshakeSuccessHandler = handshakeSuccessHandler
     }
 
-    public HandleIncoming(socket): void {
+    public HandleIncoming = (socket): void => {
         HandleHandshake(socket, this.meta, this.HandleHandshakeAck, this.handshakeSuccessHandler)
     }
 
-    private HandleHandshakeAck(serverSocket: SocketIO.Socket, remoteMeta: VoidnetNodeMeta, verify: Function) {
-        if(this.pendingHandshakes.hasOwnProperty(remoteMeta.guid)) {
-            const pendingHandshake = this.pendingHandshakes[remoteMeta.guid]
+    private HandleHandshakeAck = (serverSocket: SocketIO.Socket, remoteMeta: VoidnetNodeMeta, verify: Function) => {
+        if(this.pendingHandshakes.has(remoteMeta.guid)) {
+            const pendingHandshake = this.pendingHandshakes.get(remoteMeta.guid)
             pendingHandshake.serverSocket = serverSocket
             verify(pendingHandshake.handshakeDatum)
         } else {
@@ -156,11 +164,11 @@ class VoidnetHandshakeHandler {
         }
     }
 
-    private HandleHandshakeReslt(result: VoidnetHandshakeDatum) {
-        if(this.pendingHandshakes.hasOwnProperty(result.guid)) {
-            const pendingHandshake = this.pendingHandshakes[result.guid]
-            delete this.pendingHandshakes[result.guid]
-            if(result.extra === "success") {
+    private HandleHandshakeResult = (result: VoidnetHandshakeDatum) => {
+        if(this.pendingHandshakes.has(result.guid)) {
+            const pendingHandshake = this.pendingHandshakes.get(result.guid)
+            this.pendingHandshakes.delete(result.guid)
+            if(result.data === "success") {
                 this.handshakeSuccessHandler(pendingHandshake.ToVoidnetConnection())
             }
             else {
@@ -171,42 +179,58 @@ class VoidnetHandshakeHandler {
     }
 
     public Connect(uri: string): void {
-        const clientSocket = SocketIOClient(uri)
-        const handshakeDatum = new VoidnetHandshakeDatum(this.meta, "kek")
-        clientSocket.on("handshake-result", this.HandleHandshakeReslt)
-        clientSocket.emit("handshake", handshakeDatum, (remoteMeta: VoidnetNodeMeta) => {
-            this.pendingHandshakes[remoteMeta.guid] = new PendingHandshake(
-                clientSocket,
-                handshakeDatum,
-                remoteMeta
-            )
+        const clientSocket = SocketIOClient(uri).connect()
+        const handshakeDatum = this.meta.ToHandshakeDatum("kek")
+        clientSocket.on("handshake-result", this.HandleHandshakeResult)
+        clientSocket.on("connect", () => {
+            clientSocket.emit("handshake", handshakeDatum, (remoteMeta: VoidnetNodeMeta) => {
+                this.pendingHandshakes.set(remoteMeta.guid, new PendingHandshake(
+                    clientSocket,
+                    handshakeDatum,
+                    remoteMeta
+                ))
+            })
         })
     }
 }
 
-class VoidnetServer {
+export class VoidnetServer {
     protected server: http.Server
     protected io: SocketIO.Server
     protected handshakeHandler: VoidnetHandshakeHandler
+    protected eventEmitter: events.EventEmitter
 
-    protected meta: VoidnetNodeMeta
+    public readonly meta: VoidnetNodeMeta
     protected connections: VoidnetConnection[]
 
+    get connectionCount(): Number { return this.connections.length }
+
+
     constructor(hostname: string, port: number) {
-        this.meta = new VoidnetNodeMeta(hostname, port)
+        this.meta = new VoidnetNodeMeta({
+            hostname: hostname,
+            port: port
+        })
         this.handshakeHandler = new VoidnetHandshakeHandler(this.meta, this.HandleSuccessfullHandshake)
+        this.server = http.createServer()
         this.io = SocketIOServer(this.server)
         this.io.on("connection", this.handshakeHandler.HandleIncoming)
-        this.server = http.createServer(io)
         this.server.listen(port, hostname)
+        this.eventEmitter = new events.EventEmitter()
+        this.connections = []
     }
 
-    private HandleSuccessfullHandshake(connection: VoidnetConnection) {
+    private HandleSuccessfullHandshake = (connection: VoidnetConnection) => {
         this.connections.push(connection)
+        this.eventEmitter.emit("connection", connection)
     }
 
     public Connect(uri: string) {
         this.handshakeHandler.Connect(uri)
+    }
+
+    public on(event: string, callback: Function) {
+        this.eventEmitter.on(event, callback)
     }
 }
 
